d819c5a00a3430c1fb55d9bef5ae932c7c947e6f
oluwabusayopeyemi <olubusayopemi@gmail.com>
2026-01-03 22:08:01 +0000

    initial commit

---
+++ src/services/databaseService.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase environment variables are missing");
+}
+
+export const blogDatabase = createClient(supabaseUrl, supabaseAnonKey);
